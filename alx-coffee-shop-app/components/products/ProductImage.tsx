c476087020b83cd869cc2e55cdd235d4455a3ebb
WangilaPrish <nabwonyaprish@gmail.com>
2025-07-25 09:22:36 +0300

    fix: update ProductImage dimensions for improved layout and consistency

--- alx-coffee-shop-app/components/products/ProductImage.tsx
+++ alx-coffee-shop-app/components/products/ProductImage.tsx
@@ -9,8 +9,8 @@ export default function ProductImage({ source }: { source: any }) {
 
 const styles = StyleSheet.create({
     image: {
-        width: '90%',
-        height: RFPercentage(30),
+        width: '100%',
+        height: RFPercentage(35),
         borderRadius: RFPercentage(2),
     },
 });
